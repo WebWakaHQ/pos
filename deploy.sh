ffa9e875c1e6b6dbaf6f1d8e1e495585250d95f0
johnoko <johnoko@gmail.com>
2025-09-29 21:12:00 +0000

    first commit

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 cd /home/codeqcxa/pos.handylife.xyz
 git pull origin main
-composer install --no-dev
-php artisan migrate --force
 php artisan config:cache
-php artisan route:cache
-php artisan view:cache
